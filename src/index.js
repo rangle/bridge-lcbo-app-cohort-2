193cf31099b62e2c7be105dd54af93b70bf52b1d
jprittie <eyewallop@gmail.com>
2017-07-19 20:27:48 -0400

    Redux store and logger added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+
+import rootReducer from './redux/reducers';
+
+
+const loggerMiddleware = createLogger()
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(loggerMiddleware)
+);
+
+
+ReactDOM.render(
+  <Provider store={ store }>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
