361deaa41137d1d18cda18162765c094bbc877c7
Prtfw <shang.madeleine@gmail.com>
2017-07-20 19:35:56 -0400

    remove unused var

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Rx from 'rxjs/Rx'; //maddie: we might not need this import here
+import 'rxjs'; 
 
 
 class App extends Component {
