46a3a879276bc997f839190ef0129102ca534069
Simone Holder <simone@simoneholder.com>
2017-07-20 20:05:19 -0400

    added redux router

--- src/index.js
+++ src/index.js
@@ -10,6 +10,10 @@ import { createLogger } from 'redux-logger';
 
 import rootReducer from './redux/reducers';
 
+import { Router, Route, browserHistory } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
+
+
 
 const loggerMiddleware = createLogger()
 
@@ -19,10 +23,16 @@ const store = createStore(
   applyMiddleware(loggerMiddleware)
 );
 
+// Create an enhanced history that syncs navigation events with the store
+const history = syncHistoryWithStore(browserHistory, store)
 
 ReactDOM.render(
-  <Provider store={ store }>
-    <App />
+  <Provider store={store}>
+    { /* Tell the Router to use our enhanced history */ }
+    <Router history={history}>
+      <Route path="/" component={App}>
+      </Route>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,7 +1,10 @@
+import { routerReducer } from 'react-router-redux'
+
 import combineReducers from 'redux/es/combineReducers';
 
 
 export default combineReducers({
 
+    routing: routerReducer
 
 });
