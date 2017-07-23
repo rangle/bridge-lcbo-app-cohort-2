fe37a637e9acff794826e40c3cf91de20e1e1eca
Alex Stroulger <alexstroulger@gmail.com>
2017-07-23 18:04:04 -0400

    removing console.log

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
-const SearchBar = (props) => {
-	console.log("SEARCH BAR PROPS", props)
+const SearchBar = props => {
 	return (
 		<form onSubmit={event => {
 						event.preventDefault()
