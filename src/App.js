cf749833153f005cf0c8e4a3659d186d00e4531e
jprittie <eyewallop@gmail.com>
2017-07-23 10:13:57 -0400

    Adds wishlist panel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { connect } from 'react-redux';
+import { openWishlistPanel, closeWishlistPanel } from './redux/actions/wishlist.actions';
+import shopping_cart from './img/shopping_cart.svg';
 import './App.css';
 import 'rxjs';
 
+import Header from './components/Header';
 import ProductList from './components/ProductList';
+import WishlistPanel from './components/WishlistPanel';
 
 class App extends Component {
   render() {
+    const { wishlistIsVisible, onClickClosePanel, onClickShoppingCartLogo } = this.props;
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+        <Header onClickShoppingCartLogo={onClickShoppingCartLogo}/>
+        <WishlistPanel fakeProps={fakeProps} wishlistIsVisible={wishlistIsVisible} onClickClosePanel={onClickClosePanel}/>
         <ProductList {...fakeProps} />
       </div>
     );
   }
 }
 
-export default App;
 
 //fake props for rendering
 //should be deleted once we integrate list component with redux store
@@ -32,3 +35,14 @@ const fakeProps = {
     id: 12,
   }],
 }
+
+const mapStateToProps = state => ({
+  wishlistIsVisible: state.wishlistIsVisible,
+})
+
+const actions = {
+  onClickShoppingCartLogo: openWishlistPanel,
+  onClickClosePanel: closeWishlistPanel
+}
+
+export default connect(mapStateToProps, actions)(App);
