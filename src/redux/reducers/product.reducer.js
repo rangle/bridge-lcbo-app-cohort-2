f8f8bd9348a2baacbcf31acd5e92383b1ff7e494
Alex Stroulger <alexstroulger@gmail.com>
2017-07-25 20:50:28 -0400

    Fixes search bug

--- src/redux/reducers/product.reducer.js
+++ src/redux/reducers/product.reducer.js
@@ -1,35 +1,19 @@
 import { PRODUCT_ACTIONS } from '../actions/product.actions';
-
-// const DEFAULT_STATE = {
-//   wishList: [],
-//   productList: [],
-// };
-
-const fakeProps = {
+import {SEND_API_RESULTS} from '../actions/';
+const DEFAULT_STATE = {
   wishList: [],
-  productList: [
-  {
-    name: 'item 1',
-    price_in_cents: 374,
-    image_thumb_url: 'http://placekitten.com/g/80/120',
-    id: 1,
-  },
-  {
-    name: 'item 2',
-    price_in_cents: 800,
-    image_thumb_url: 'http://placekitten.com/g/80/120',
-    id: 2,
-  },
-  ],
-}
+  productList: [],
+};
 
 // if (localStorage.getItem("state") !== null) {
 //   //set this to DEFAULT_STATE when we remove fakeProps
 //   fakeProps.wishList = JSON.parse(localStorage.getItem("state")).wishList;
 // }
 
-export default (state = fakeProps, action) => {
+export default (state = DEFAULT_STATE, action) => {
   switch(action.type) {
+     case SEND_API_RESULTS:
+        return { productList: action.payload }
 
     case PRODUCT_ACTIONS.ADD_PRODUCT_TO_WISHLIST:
       return {...state,
