3190764e04fff5a0016d3d5758c5f266db1a0b50
jprittie <eyewallop@gmail.com>
2017-07-27 20:11:56 -0400

    Adds button and header styling (#20)
    * Adds button and header styling
    
    * Fixes review issues
    
    * Adds more review fixes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import './Header.css'
 import shopping_cart from '../../img/shopping_cart.svg'
+import cheers from '../../img/cheers.jpg'
 import SearchBar from '../SearchBar'
+
+const headerImageStyle = {
+	width: "100%",
+  backgroundImage: ` url(${cheers}) `,
+  backgroundSize: 'cover',
+  overflow: 'hidden'
+}
 
 const Header = props => {
 	return (
-	  <div className="header">
+	  <div className="header" style={ headerImageStyle }>
 	    <img src={shopping_cart} className="shopping-cart-logo" alt="open wishlist button" onClick={ () => props.onClickShoppingCartLogo()} />
-	    <div className="text-input">
+	    <div className="header-text-input-group">
 			<SearchBar updateSearchText={props.updateSearchText} searchString={props.searchString} getResults={props.getResults}/>
 		</div>
 	  </div>
