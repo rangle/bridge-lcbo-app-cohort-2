62310bf2013e37c8315a78aa4d5a7d698921528e
Alex Stroulger <alexstroulger@gmail.com>
2017-07-30 08:55:19 -0400

    PR fixes and formatting

--- src/components/CartButton/CartButton.js
+++ src/components/CartButton/CartButton.js
@@ -1,22 +1,16 @@
-import React from 'react'
-import shopping_cart from '../../img/shopping_cart.svg'
-import './CartButton.css'
+import React from "react"
+import "./CartButton.css"
 
-const cartImageStyle = {
-	width: "40px",
-	height: "40px",
-  backgroundImage: `url(${shopping_cart})`,
-  backgroundSize: 'cover',
-}
-
-const CartButton = props  => {
-	return (
-		<div className="cart-wrapper" style={cartImageStyle} onClick={() => props.onClick()} >
-		{(props.wishList && props.wishList.length) > 0 ?
-			<div className="red-badge">{props.wishList.length}</div>
-			: null}
-		</div>
-	)
+const CartButton = props => {
+  return (
+    <div className="cart-wrapper" onClick={() => props.onClick()}>
+      {(props.wishList && props.wishList.length) > 0
+        ? <div className="red-badge">
+            {props.wishList.length}
+          </div>
+        : null}
+    </div>
+  )
 }
 
 export default CartButton
