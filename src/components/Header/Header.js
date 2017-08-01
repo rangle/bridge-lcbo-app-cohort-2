eb1dd055c701c84718f93ade49ff012ca60562f0
Alex Stroulger <astroulger@theworkinggroup.ca>
2017-08-01 08:23:41 -0400

    Adds tests for header reducer (#24)
    * Adds tests for header reducer
    
    * slight refactor to header and some formatting
    
    * removes console log
    
    * removes unnecessary reducer import

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,25 +1,32 @@
-import React from 'react'
-import './Header.css'
-import cheers from '../../img/cheers.jpg'
-import SearchBar from '../SearchBar'
-import CartButton from '../CartButton'
+import React from "react"
+import "./Header.css"
+import cheers from "../../img/cheers.jpg"
+import SearchBar from "../SearchBar"
+import CartButton from "../CartButton"
 
 const headerImageStyle = {
-	width: "100%",
+  width: "100%",
   backgroundImage: `url(${cheers})`,
-  backgroundSize: 'cover',
-  overflow: 'hidden'
+  backgroundSize: "cover",
+  overflow: "hidden"
 }
 
 const Header = props => {
-	return (
-	  <div className="header" style={ headerImageStyle }>
-	  <CartButton wishList={props.wishList} onClick={() => props.onClickShoppingCartLogo()}/>
-	    <div className="header-text-input-group">
-			<SearchBar updateSearchText={props.updateSearchText} searchString={props.searchString} getResults={props.getResults}/>
-		</div>
-	  </div>
-	)
+  return (
+    <div className="header" style={headerImageStyle}>
+      <CartButton
+        wishList={props.wishList}
+        onClick={() => props.onClickShoppingCartLogo()}
+      />
+      <div className="header-text-input-group">
+        <SearchBar
+          updateSearchText={props.updateSearchText}
+          searchString={props.searchString}
+          getResults={props.getResults}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default Header
