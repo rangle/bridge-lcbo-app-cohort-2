97b28ae25de681a8a6be75be1e259fa1d70af49e
jprittie <eyewallop@gmail.com>
2017-08-03 18:12:09 -0400

    Tests wishlist reducer

---
+++ src/redux/reducers/wishlist.reducer.test.js
@@ -0,0 +1,32 @@
+import {wishlistIsVisible} from "./wishlist.reducer"
+import {WISHLIST_ACTIONS} from "../actions/wishlist.actions"
+
+describe("wishlist panel reducer", () => {
+  let wishlistReducerState = wishlistIsVisible(false, {
+    type: "foo",
+    payload: ""
+  })
+
+  it("should have an initial state of false", () => {
+    expect(wishlistReducerState).toBeFalsy()
+  })
+
+  it("should be false when the panel is closed", () => {
+    expect(wishlistReducerState).toBe(false)
+
+    wishlistReducerState = wishlistIsVisible(wishlistReducerState, {
+      type: WISHLIST_ACTIONS.CLOSE_PANEL,
+      payload: ""
+    })
+    expect(wishlistReducerState).toBe(false)
+  })
+
+  it("should be true when the panel is open", () => {
+    expect(wishlistReducerState).toBe(false)
+    wishlistReducerState = wishlistIsVisible(wishlistReducerState, {
+      type: WISHLIST_ACTIONS.OPEN_PANEL,
+      payload: ""
+    })
+    expect(wishlistReducerState).toBe(true)
+  })
+})
