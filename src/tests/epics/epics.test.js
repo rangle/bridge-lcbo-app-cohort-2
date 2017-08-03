e7198eae63f85883cfcb20248dfc55132b5980bb
Madeleine Shang <shang.madeleine@gmail.com>
2017-08-03 11:17:16 -0400

    Update epics.test.js

--- src/tests/epics/epics.test.js
+++ src/tests/epics/epics.test.js
@@ -1,33 +1,28 @@
-import { ActionsObservable } from 'redux-observable';
+import { ActionsObservable } from "redux-observable";
 
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/of';
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/observable/of";
 
-import { apiFetchEpic } from '../../epics';
-import { getAPIResults, SEND_API_RESULTS } from '../../redux/actions';
-
+import { apiFetchEpic } from "../../epics";
+import { getAPIResults, SEND_API_RESULTS } from "../../redux/actions";
 
 // this creates an action observable, allowing us to fake an action that our epic can hear
-const action$ = ActionsObservable.of(
-  getAPIResults('socks')
-);
+const action$ = ActionsObservable.of(getAPIResults("socks"));
 
+describe("getRecipeByName Epic", () => {
+  it("dispatches the correct action when it is successful", done => {
+    const ajax = () =>
+      Observable.of({
+        response: {
+          result: ["socks", "mocks"],
+        },
+      }); // this will be replacing our ajax call!
 
-describe('getRecipeByName Epic', () => {
+    apiFetchEpic(action$, null, { ajax }).subscribe(result => {
+      expect(result.type).toBe(SEND_API_RESULTS);
+      expect(result.payload).toEqual(["socks", "mocks"]);
 
-  it('dispatches the correct action when it is successful', (done) => {
-    const ajax = () => Observable.of({ response: {
-      result: ['socks', 'mocks']
-    }}); // this will be replacing our ajax call!
-
-
-    apiFetchEpic(action$, null, { ajax })
-      .subscribe(result => {
-        expect(result.type).toBe(SEND_API_RESULTS);
-        expect(result.payload).toEqual(['socks', 'mocks']);
-
-        done();
-      })
-
+      done();
+    });
   });
 });
