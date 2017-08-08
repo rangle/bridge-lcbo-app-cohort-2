f42eed38c8c1ec0e8e8ca260f72940fe0d799626
Alex Stroulger <alexstroulger@gmail.com>
2017-08-08 19:40:17 -0400

    updates default state

--- src/redux/reducers/storeLocation.reducer.js
+++ src/redux/reducers/storeLocation.reducer.js
@@ -1,11 +1,16 @@
 import { STORE_LOCATION_ACTIONS } from "../actions/storeLocation.actions"
 
-const DEFAULT_STATE = []
+const DEFAULT_STATE = {
+  storeList: []
+}
 
 export const storeLocationReducer = (state = DEFAULT_STATE, action) => {
   switch (action.type) {
     case STORE_LOCATION_ACTIONS.SEND_STORES_BY_PRODUCT_IDS:
-      return [...action.payload]
+      return {
+        ...state,
+        storeList: action.payload
+      }
     default:
       return state
   }
