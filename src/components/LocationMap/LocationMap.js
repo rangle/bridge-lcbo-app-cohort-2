e4cf07ac5c8ce20a0bc54c71554cd830456b3ba0
Simone Holder <simone@simoneholder.com>
2017-08-09 23:34:57 -0400

    adds google library

---
+++ src/components/LocationMap/LocationMap.js
@@ -0,0 +1,18 @@
+import { default as React, Component } from "react"
+
+import { withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+
+const simpleMap = withGoogleMap(props =>
+  <GoogleMap defaultZoom={8} defaultCenter={{ lat: 43.6532, lng: 79.3832 }} />
+) // Toronto lat/long coordinates
+
+export default class LocationMap extends Component {
+  render() {
+    return (
+      <simpleMap
+        containerElement={<div style={{ height: `100%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    )
+  }
+}
--- src/containers/StoreFinder.container.js
+++ src/containers/StoreFinder.container.js
@@ -1,24 +1,30 @@
 import React from "react"
 import StoresList from "../components/StoresList"
+import LocationMap from "../components/LocationMap"
+import { withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 import { bindActionCreators } from "redux"
 import { connect } from "react-redux"
 import { getStoresByProductIDs } from "../redux/actions/storeLocation.actions"
 
-const StoreFinder = props => <StoresList {...props} />
+const StoreFinder = props =>
+	<div>
+		<StoresList {...props} />
+		<LocationMap {...props} />
+	</div>
 
 const mapStateToProps = state => {
-  return {
-    storeList: state.storesList.storeList
-  }
+	return {
+		storeList: state.storesList.storeList
+	}
 }
 
 const mapDispatchToProps = dispatch => {
-  return bindActionCreators(
-    {
-      getStoresByProductIDs
-    },
-    dispatch
-  )
+	return bindActionCreators(
+		{
+			getStoresByProductIDs
+		},
+		dispatch
+	)
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(StoreFinder)
