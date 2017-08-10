82a11d1280f8c9f246adafe12e10b09906d5e336
Simone Holder <simone@simoneholder.com>
2017-08-09 23:48:07 -0400

    revised version of previous

--- src/containers/StoreFinder.container.js
+++ src/containers/StoreFinder.container.js
@@ -1,7 +1,6 @@
 import React from "react"
 import StoresList from "../components/StoresList"
 import LocationMap from "../components/LocationMap"
-import { withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 import { bindActionCreators } from "redux"
 import { connect } from "react-redux"
 import { getStoresByProductIDs } from "../redux/actions/storeLocation.actions"
