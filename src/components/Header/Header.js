ddf416293ba8048c4d689c26eb6c1883d9142038
Simone Holder <simone@simoneholder.com>
2017-08-11 14:02:20 -0400

    style header image with dark overlay

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,16 +4,19 @@ import cheers from "../../img/cheers.jpg"
 import SearchBar from "../SearchBar"
 import CartButton from "../CartButton"
 
-const headerImageStyle = {
-  width: "100%",
-  backgroundImage: `url(${cheers})`,
-  backgroundSize: "cover",
-  overflow: "hidden"
-}
+// const headerImageStyle = {
+//   width: "100%",
+//   backgroundImage: `url(${cheers})`,
+//   background: "linear-gradient(0deg,rgba(0,0,0,.7),rgba(0,0,0,.7))",
+//   // backgroundImage: `url(${cheers})`,
+//   backgroundSize: "cover",
+//   overflow: "hidden"
+// }
 
 const Header = props => {
   return (
-    <div className="header" style={headerImageStyle}>
+    <div className="header">
+      <h1>LCBO App</h1>
       <CartButton
         wishList={props.wishList}
         onClick={() => props.onClickShoppingCartLogo()}
