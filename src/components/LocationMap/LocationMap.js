96d2e857c60df3bde669a9c8f3aaed362a2c088c
Simone Holder <simone@simoneholder.com>
2017-08-11 14:03:37 -0400

    Feature/location map rendered (#43)
    * adds google library
    
    * react-google-maps added
    
    * revised version of previous
    
    * rendering on screengit add . in russian though
    
    * map is renderinggit add .
    
    * improved map placement
    
    * improved map functionality thanks to Della

---
+++ src/components/LocationMap/LocationMap.js
@@ -0,0 +1,60 @@
+import { default as React, Component } from "react"
+
+import { withGoogleMap, GoogleMap } from "react-google-maps"
+
+const TOR_LAT_LNG = { lat: 43.6532, lng: -79.3832 } // Toronto lat/long coordinates
+
+const SimpleMap = withGoogleMap(props =>
+	<GoogleMap
+		ref={res => props.onLoad(res)}
+		defaultZoom={16}
+		defaultCenter={TOR_LAT_LNG}
+	/>
+)
+
+export default class LocationMap extends Component {
+	componentDidMount(nextProps) {
+		navigator.geolocation.getCurrentPosition(position => {
+			const latLng = {
+				lat: position.coords.latitude,
+				lng: position.coords.longitude
+			}
+
+			this.mapLib.panTo(latLng)
+		})
+	}
+
+	render() {
+		return (
+			<SimpleMap
+				onLoad={res => (this.mapLib = res)}
+				containerElement={
+					<div
+						style={{
+							position: "absolute",
+							height: `100%`,
+							width: `70%`,
+							top: 0,
+							left: `30em`,
+							right: 0,
+							bottom: 0,
+							justifyContent: "flex-end",
+							alignItems: "center"
+						}}
+					/>
+				}
+				mapElement={
+					<div
+						style={{
+							position: "absolute",
+							top: 0,
+							left: 0,
+							right: 0,
+							bottom: 0
+						}}
+					/>
+				}
+			/>
+		)
+	}
+}
