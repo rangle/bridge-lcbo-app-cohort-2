663ba15bbdcac6e1a7f9aaa6fafea89c42c5907d
Jocelyn Jeffrey <jocelynjeffrey@gmail.com>
2017-08-13 13:39:07 -0400

    added go back button from map screen

--- src/containers/StoreFinder.container.js
+++ src/containers/StoreFinder.container.js
@@ -4,9 +4,14 @@ import LocationMap from "../components/LocationMap"
 import { bindActionCreators } from "redux"
 import { connect } from "react-redux"
 import { getStoresByProductIDs } from "../redux/actions/storeLocation.actions"
+import { Link } from "react-router"
+import "./StoreFinder.container.css"
 
 const StoreFinder = props =>
-	<div>
+	<div className="storeFinder-container">
+		<Link className="link" to="/">
+			&larr; go back
+		</Link>
 		<StoresList {...props} />
 		<LocationMap />
 	</div>
