66e8ccec7e1eff216d28ff586d6c258f7dc4b155
Alex Stroulger <astroulger@theworkinggroup.ca>
2017-08-17 18:16:50 -0400

    [FEATURE] Updates API call for stores list (#50)
    * [FEATURE] Updates API call
     * Adding tests for getStoreLocations Epic
     * renames and moves a couple files for organization
    * removes unnecessary import

--- src/redux/epics/getStoreLocations.epic.js
+++ src/redux/epics/getStoreLocations.epic.js
@@ -5,13 +5,16 @@ import {
 
 import { LCBO_API_KEY, LCBO_BASE_URL } from "../constants"
 
+const productIDs = ids => ids.join(",")
+
 export const getStoreLocations = (action$, _, { ajax }) =>
   action$
     .ofType(STORE_LOCATION_ACTIONS.GET_STORES_BY_PRODUCT_IDS)
     .mergeMap(action =>
       ajax(
-        `${LCBO_BASE_URL}stores?product_id=${action
-          .payload[0]}&lat=43.659&lon=-79.439&access_key=${LCBO_API_KEY}`
+        `${LCBO_BASE_URL}stores?products=${productIDs(
+          action.payload
+        )}&lat=43.653&lon=-79.383&access_key=${LCBO_API_KEY}`
       )
     )
     .map(({ response }) => {
--- src/tests/reducers/wishlist.reducer.test.js
+++ src/tests/reducers/wishlist.reducer.test.js
@@ -1,5 +1,5 @@
-import { wishlistIsVisible } from "./wishlist.reducer"
-import { WISHLIST_ACTIONS } from "../actions/wishlist.actions"
+import { wishlistIsVisible } from "../../redux/reducers/wishlist.reducer"
+import { WISHLIST_ACTIONS } from "../../redux/actions/wishlist.actions"
 
 describe("wishlist panel reducer", () => {
   let wishlistReducerState = wishlistIsVisible(false, {
