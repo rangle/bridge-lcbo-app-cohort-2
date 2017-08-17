9f965551f03a9c519add72501a37b4dea819155c
Alex Stroulger <alexstroulger@gmail.com>
2017-08-16 22:36:38 -0400

    [FEATURE] Updates API call  * Adding tests for getStoreLocations Epic  * renames and moves a couple files for organization

--- src/redux/actions/storeLocation.actions.js
+++ src/redux/actions/storeLocation.actions.js
@@ -3,9 +3,9 @@ export const STORE_LOCATION_ACTIONS = {
   SEND_STORES_BY_PRODUCT_IDS: "SEND_STORES_BY_PRODUCT_IDS"
 }
 
-export const getStoresByProductIDs = () => ({
+export const getStoresByProductIDs = ids => ({
   type: STORE_LOCATION_ACTIONS.GET_STORES_BY_PRODUCT_IDS,
-  payload: [311787]
+  payload: [311787, 109017, 432732]
 })
 
 export const sendStoresByProductIDs = storesList => {
---
+++ src/tests/epics/getStoreLocations.epic.test.js
@@ -0,0 +1,37 @@
+import { ActionsObservable } from "redux-observable"
+import "rxjs"
+import { Observable } from "rxjs/Observable"
+import "rxjs/add/observable/of"
+
+import { getStoreLocations } from "../../redux/epics/getStoreLocations.epic"
+import {
+  getStoresByProductIDs,
+  sendStoresByProductIDs,
+  STORE_LOCATION_ACTIONS
+} from "../../redux/actions/storeLocation.actions"
+
+// this creates an action observable, allowing us to fake an action that our epic can hear
+const action$ = ActionsObservable.of(getStoresByProductIDs([1, 2, 3]))
+
+describe("getStoreLocations Epic", () => {
+  it("dispatches the correct action when it is successful", done => {
+    const ajax = () =>
+      Observable.of({
+        response: {
+          result: [{ name: "store 1", id: 5 }, { name: "store 2", id: 9056 }]
+        }
+      })
+
+    getStoreLocations(action$, null, { ajax }).subscribe(result => {
+      expect(result.type).toBe(
+        STORE_LOCATION_ACTIONS.SEND_STORES_BY_PRODUCT_IDS
+      )
+      expect(result.payload).toEqual([
+        { name: "store 1", id: 5 },
+        { name: "store 2", id: 9056 }
+      ])
+
+      done()
+    })
+  })
+})
