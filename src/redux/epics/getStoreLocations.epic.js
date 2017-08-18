040b6e20688edb79d34ac30263380763be10e0b7
Prtfw <shang.madeleine@gmail.com>
2017-08-17 20:14:55 -0400

    final tweak

--- src/redux/epics/getStoreLocations.epic.js
+++ src/redux/epics/getStoreLocations.epic.js
@@ -1,4 +1,4 @@
-import Rx from 'rxjs';
+import Rx from "rxjs";
 
 import {
   STORE_LOCATION_ACTIONS,
@@ -8,44 +8,33 @@ import {
 
 import { LCBO_API_KEY, LCBO_BASE_URL } from "../constants";
 
-const productIDs = ids => ids.join(",")
+const productIDs = ids => ids.join(",");
 
 export const getStoreLocations = (action$, _, { ajax }) =>
   action$
     .ofType(STORE_LOCATION_ACTIONS.GET_STORES_BY_PRODUCT_IDS)
     .mergeMap(action => {
-      console.log(15, `${LCBO_BASE_URL}stores?product_id=${action
-        .payload.id}&lat=${action
-          .payload.latLng.lat}&lon=${action
-            .payload.latLng.lng}&access_key=${LCBO_API_KEY}`)
-
       return ajax(
-        `${LCBO_BASE_URL}stores?product_id=${action
-          .payload.id}&lat=${action
-            .payload.latLng.lat}&lon=${action
-              .payload.latLng.lng}&access_key=${LCBO_API_KEY}`,
-      )
-    }
-    )
+        `${LCBO_BASE_URL}stores?products=${productIDs(action.payload)}&lat=${action.payload.latLng
+          .lat}&lon=${action.payload.latLng.lng}&access_key=${LCBO_API_KEY}`,
+      );
+    })
     .map(({ response }) => {
       return sendStoresByProductIDs(response.result);
     });
 
-
 function getPreciseLocation() {
-      return new Promise(function (resolve, reject) {
-  navigator.geolocation.getCurrentPosition(function (position) {
-    resolve({lat:position.coords.latitude, lng: position.coords.longitude});
+  return new Promise(function(resolve, reject) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      resolve({ lat: position.coords.latitude, lng: position.coords.longitude });
+    });
   });
-});
 }
-
 
 export const getUserCurrentLocation = (action$, _, { ajax }) =>
   action$
     .ofType(STORE_LOCATION_ACTIONS.GET_USER_CURRENT_LOCATION)
-    .mergeMap(action =>
-          Rx.Observable.fromPromise(getPreciseLocation())
-  ).map(({ lat, lng }) => {
+    .mergeMap(action => Rx.Observable.fromPromise(getPreciseLocation()))
+    .map(({ lat, lng }) => {
       return setUserLocation({ lat, lng });
     });
