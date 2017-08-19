e446eec8cfba815e5614b8d53e25bd79a5075dbe
Prtfw <shang.madeleine@gmail.com>
2017-08-19 18:12:19 -0400

    remove semis

--- src/redux/epics/getStoreLocations.epic.js
+++ src/redux/epics/getStoreLocations.epic.js
@@ -1,14 +1,14 @@
-import Rx from "rxjs";
+import Rx from "rxjs"
 
 import {
   STORE_LOCATION_ACTIONS,
   sendStoresByProductIDs,
   setUserLocation,
-} from "../actions/storeLocation.actions";
+} from "../actions/storeLocation.actions"
 
-import { LCBO_API_KEY, LCBO_BASE_URL } from "../constants";
+import { LCBO_API_KEY, LCBO_BASE_URL } from "../constants"
 
-const productIDs = (ids = []) => ids.join(",");
+const productIDs = (ids = []) => ids.join(",")
 
 export const getStoreLocations = (action$, _, { ajax }) =>
   action$
@@ -17,20 +17,20 @@ export const getStoreLocations = (action$, _, { ajax }) =>
       return ajax(
         `${LCBO_BASE_URL}stores?products=${productIDs(action.payload.ids)}&lat=${action.payload
           .latLng.lat}&lon=${action.payload.latLng.lng}&access_key=${LCBO_API_KEY}`,
-      );
+      )
     })
     .map(({ response }) => {
-      return sendStoresByProductIDs(response.result);
-    });
+      return sendStoresByProductIDs(response.result)
+    })
 
 function getPreciseLocation() {
   return new Promise(function(resolve, reject) {
     navigator.geolocation.getCurrentPosition(position => {
       position
         ? resolve({ lat: position.coords.latitude, lng: position.coords.longitude })
-        : reject("no value for position");
-    });
-  });
+        : reject("no value for position")
+    })
+  })
 }
 
 export const getUserCurrentLocation = (action$, _, { ajax }) =>
@@ -38,5 +38,5 @@ export const getUserCurrentLocation = (action$, _, { ajax }) =>
     .ofType(STORE_LOCATION_ACTIONS.GET_USER_CURRENT_LOCATION)
     .mergeMap(action => Rx.Observable.fromPromise(getPreciseLocation()))
     .map(({ lat, lng }) => {
-      return setUserLocation({ lat, lng });
-    });
+      return setUserLocation({ lat, lng })
+    })
