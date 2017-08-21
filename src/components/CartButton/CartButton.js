082e6cf5f756e6a8ff2e3b4fdf4d457f8508c230
Prtfw <shang.madeleine@gmail.com>
2017-08-21 17:57:42 -0400

    add animation working

--- src/components/CartButton/CartButton.js
+++ src/components/CartButton/CartButton.js
@@ -1,15 +1,30 @@
 import React from "react"
 import "./CartButton.css"
+import { CSSTransitionGroup } from "react-transition-group";
+
 
 const CartButton = props => {
 	return (
-		<div className="cart-wrapper" onClick={() => props.onClick()}>
+		<CSSTransitionGroup
+			component="div"
+			className="cart-wrapper"
+			transitionName="cart-wrapper"
+			transitionEnterTimeout={800}
+			transitionLeaveTimeout={800}
+			>
+		<span key={props.wishList.length}>
+		<div  onClick={() => props.onClick()}>
+			{/* className="cart-wrapper" */}
+
 			{(props.wishList && props.wishList.length) > 0
 				? <div className="red-badge">
 						{props.wishList.length}
 					</div>
 				: null}
+
 		</div>
+		</span>
+	</CSSTransitionGroup>
 	)
 }
 
