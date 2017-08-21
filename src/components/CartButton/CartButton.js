dffe1c8bedf199cd40db7adbf2727e6be4c58aa0
Prtfw <shang.madeleine@gmail.com>
2017-08-21 18:04:43 -0400

    get panel working again and format

--- src/components/CartButton/CartButton.js
+++ src/components/CartButton/CartButton.js
@@ -1,31 +1,28 @@
-import React from "react"
-import "./CartButton.css"
+import React from "react";
+import "./CartButton.css";
 import { CSSTransitionGroup } from "react-transition-group";
 
-
 const CartButton = props => {
-	return (
-		<CSSTransitionGroup
-			component="div"
-			className="cart-wrapper"
-			transitionName="cart-wrapper"
-			transitionEnterTimeout={800}
-			transitionLeaveTimeout={800}
-			>
-		<span key={props.wishList.length}>
-		<div  onClick={() => props.onClick()}>
-			{/* className="cart-wrapper" */}
+  return (
+    <CSSTransitionGroup
+      component="div"
+      className="cart-wrapper"
+      transitionName="cart-wrapper"
+      transitionEnterTimeout={800}
+      transitionLeaveTimeout={800}
+      onClick={() => props.onClick()}
+    >
+      <span key={props.wishList.length}>
+        <div>
+          {(props.wishList && props.wishList.length) > 0
+            ? <div className="red-badge">
+                {props.wishList.length}
+              </div>
+            : null}
+        </div>
+      </span>
+    </CSSTransitionGroup>
+  );
+};
 
-			{(props.wishList && props.wishList.length) > 0
-				? <div className="red-badge">
-						{props.wishList.length}
-					</div>
-				: null}
-
-		</div>
-		</span>
-	</CSSTransitionGroup>
-	)
-}
-
-export default CartButton
+export default CartButton;
