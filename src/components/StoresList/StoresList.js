5cf48ec995cfdd9f8afe24dca9f54db1a2df0260
Jocelyn Jeffrey <jocelynjeffrey@gmail.com>
2017-08-20 20:43:57 -0400

    fixed failing warning for equality operator

--- src/components/StoresList/StoresList.js
+++ src/components/StoresList/StoresList.js
@@ -24,7 +24,7 @@ const StoresList = props => {
   function msmTo12time(msm) {
     const time = msmTo24time(msm)
     const h24 = time[0]
-    const h12 = 0 == h24 ? 12 : h24 > 12 ? h24 - 10 - 2 : h24
+    const h12 = 0 === h24 ? 12 : h24 > 12 ? h24 - 10 - 2 : h24
     const ampm = h24 >= 12 ? "PM" : "AM"
     const twoDigitMin = time[1] < 10 ? `:${time[1]}0` : `:${time[1]}`
 
