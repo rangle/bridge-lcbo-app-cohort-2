ca0b06e22fc56c236f8dbd44c797788b2aad78c6
Madeleine Shang <shang.madeleine@gmail.com>
2017-08-22 20:10:59 -0400

    Visual indication (#55)
    * add animation working
    
    * get panel working again and format
    
    * clean up unneeded code
    
    * guard null wishlist
    
    * Update CartButton.js
    
    * Update CartButton.js

--- src/components/CartButton/CartButton.js
+++ src/components/CartButton/CartButton.js
@@ -1,16 +1,28 @@
 import React from "react"
 import "./CartButton.css"
+import { CSSTransitionGroup } from "react-transition-group"
 
 const CartButton = props => {
-	return (
-		<div className="cart-wrapper" onClick={() => props.onClick()}>
-			{(props.wishList && props.wishList.length) > 0
-				? <div className="red-badge">
-						{props.wishList.length}
-					</div>
-				: null}
-		</div>
-	)
-}
+  return (
+    <CSSTransitionGroup
+      component="div"
+      className="cart-wrapper"
+      transitionName="cart-wrapper"
+      transitionEnterTimeout={800}
+      transitionLeaveTimeout={800}
+      onClick={() => props.onClick()}
+    >
+      <span key={(props.wishList && props.wishList.length) || 0}>
+        <div>
+          {(props.wishList && props.wishList.length) > 0
+            ? <div className="red-badge">
+                {props.wishList.length}
+              </div>
+            : null}
+        </div>
+      </span>
+    </CSSTransitionGroup>
+  );
+};
 
 export default CartButton
